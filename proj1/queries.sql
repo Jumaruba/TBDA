37e005c16834a29ebee35f45bbfe951182177d1f
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-04-03 18:25:49 +0100

    Add: table x question 3 and 4

--- proj1/queries.sql
+++ proj1/queries.sql
@@ -17,3 +17,53 @@ JOIN xtiposaula t on o.codigo=t.codigo and t.periodo= o.periodo and o.ano_letivo
 JOIN xdsd d on d.id=t.id
 WHERE u.curso=233 and o.ano_letivo='2004/2005'
 GROUP BY t.tipo;
+
+
+
+-- QUESTIONS 3: Which courses (show the code) did have occurrences planned but did 
+-- not get service assigned in year 2003/2004?
+-- a) Use not in.
+SELECT UNIQUE(u.codigo)
+FROM xucs u 
+JOIN xocorrencias o ON u.codigo=o.codigo
+WHERE u.codigo NOT IN (
+    SELECT o.codigo 
+    FROM xocorrencias o 
+    WHERE o.ano_letivo='2003/2004'
+); 
+
+-- b) Use external join and is null.
+
+CREATE VIEW occurrences AS 
+SELECT o.codigo 
+FROM xocorrencias o 
+WHERE o.ano_letivo='2003/2004'; 
+
+SELECT UNIQUE(o.codigo)
+FROM xocorrencias ox
+FULL OUTER JOIN occurrences o ON o.codigo=ox.codigo;
+
+
+DROP VIEW occurrences;
+
+-- QUESTION 4
+CREATE VIEW docente_horas AS 
+SELECT doc.nr, SUM(d.horas) as sum_horas, t.tipo, doc.nome
+FROM xdocentes doc
+JOIN xdsd d ON doc.nr=d.nr
+JOIN xtiposaula t ON d.id=t.id
+WHERE t.ano_letivo='2003/2004'
+GROUP BY doc.nome,doc.nr,t.tipo;
+
+
+CREATE VIEW max_horas_tipo AS
+SELECT MAX(sum_horas) as sum_horas, dh.tipo
+FROM docente_horas dh 
+GROUP BY dh.tipo;
+
+SELECT dh.nr,dh.nome,dh.tipo,mht.sum_horas
+FROM max_horas_tipo mht 
+JOIN docente_horas dh ON dh.sum_horas=mht.sum_horas AND dh.tipo=mht.tipo;
+
+DROP VIEW docente_horas;
+DROP VIEW max_horas_tipo;
