e0d95049cf8698a837e89bb9bf3e569a066b91e0
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-04-03 17:22:51 +0100

    Add: table x question 2

--- proj1/queries.sql
+++ proj1/queries.sql
@@ -5,3 +5,15 @@ FROM xucs u
 JOIN xocorrencias o on u.codigo=o.codigo
 JOIN xtiposaula t on o.codigo=t.codigo and t.periodo= o.periodo and o.ano_letivo=t.ano_letivo
 WHERE u.designacao='Bases de Dados' and u.curso=275;
+
+
+
+-- QUESTIONS 2
+
+SELECT t.tipo, SUM(d.horas)
+FROM xucs u 
+JOIN xocorrencias o on u.codigo=o.codigo
+JOIN xtiposaula t on o.codigo=t.codigo and t.periodo= o.periodo and o.ano_letivo=t.ano_letivo
+JOIN xdsd d on d.id=t.id
+WHERE u.curso=233 and o.ano_letivo='2004/2005'
+GROUP BY t.tipo;
