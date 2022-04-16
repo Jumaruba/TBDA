7b7b0899d055a84bc469c83de785e9227001d8ef
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-04-16 22:31:50 +0100

    fix: question 5 and question 2

--- proj1/queries.sql
+++ proj1/queries.sql
@@ -10,7 +10,7 @@ WHERE u.designacao='Bases de Dados' and u.curso=275;
 
 -- QUESTIONS 2
 
-SELECT t.tipo, SUM(d.horas)
+SELECT t.tipo,  SUM(d.horas*t.n_aulas) as horas
 FROM xucs u 
 JOIN xocorrencias o on u.codigo=o.codigo
 JOIN xtiposaula t on o.codigo=t.codigo and t.periodo= o.periodo and o.ano_letivo=t.ano_letivo
@@ -70,7 +70,7 @@ DROP VIEW max_horas_tipo;
 
 -- QUESTION 5 
 
-SELECT u.curso, o.ano_letivo, o.periodo, SUM(d.horas)
+SELECT u.codigo, u.curso, o.ano_letivo, o.periodo, SUM(d.horas)
 FROM xucs u 
 JOIN xocorrencias o on u.codigo=o.codigo
 JOIN xtiposaula t on o.codigo=t.codigo and t.periodo= o.periodo and o.ano_letivo=t.ano_letivo
