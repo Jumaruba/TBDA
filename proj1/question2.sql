789d77f9d4b8f6e2c530049326ea216155666053
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-04-17 14:45:16 +0100

    fix: change queries 2 and 4

--- proj1/question2.sql
+++ proj1/question2.sql
@@ -3,10 +3,9 @@ DROP INDEX TIPOSAULA_IDX;
 CREATE INDEX UCS_IDX ON ZUCS(CODIGO, CURSO);  
 CREATE INDEX TIPOSAULA_IDX ON ZTIPOSAULA(ANO_LETIVO);
 
-SELECT t.tipo, SUM(t.horas_turno * COALESCE(n_aulas,1)) as horas
+SELECT t.tipo, SUM(t.horas_turno * COALESCE(t.n_aulas,1) * COALESCE(t.turnos,1)) as horas
 FROM xucs u 
 JOIN xocorrencias o on u.codigo=o.codigo
 JOIN xtiposaula t on o.codigo=t.codigo and t.periodo=o.periodo and o.ano_letivo=t.ano_letivo
-JOIN xdsd d on d.id=t.id
 WHERE u.curso=233 and o.ano_letivo='2004/2005'
 GROUP BY t.tipo;
--- proj1/question5.sql
+++ proj1/question5.sql
@@ -19,7 +19,7 @@ WHERE segment_name='TIPOSAULA_IDX';
 
 -- Y 
 
-SELECT u.codigo, u.curso, o.ano_letivo, o.periodo, SUM(d.horas)
+SELECT u.codigo, u.curso, o.ano_letivo, o.periodo, SUM(t.horas_turno * COALESCE(t.n_aulas,1) * COALESCE(t.turnos, 1))
 FROM yucs u 
 JOIN yocorrencias o ON u.codigo=o.codigo
 JOIN ytiposaula t ON o.codigo=t.codigo AND t.periodo= o.periodo and o.ano_letivo=t.ano_letivo
@@ -30,12 +30,10 @@ GROUP BY u.curso, o.ano_letivo, o.periodo, u.codigo;
 
 -- Z 
 
-
-SELECT u.codigo, u.curso, o.ano_letivo, o.periodo, SUM(d.horas)
+SELECT u.codigo, u.curso, o.ano_letivo, o.periodo, SUM(t.horas_turno * COALESCE(t.n_aulas,1) * COALESCE(t.turnos, 1)) as horas_totais
 FROM zucs u 
 JOIN zocorrencias o ON u.codigo=o.codigo
 JOIN ztiposaula t ON o.codigo=t.codigo AND t.periodo= o.periodo and o.ano_letivo=t.ano_letivo
 JOIN zdsd d ON d.id=t.id
 WHERE t.tipo='OT' AND (t.ano_letivo='2002/2003' OR t.ano_letivo='2003/2004')
 GROUP BY u.curso, o.ano_letivo, o.periodo, u.codigo; 
-
