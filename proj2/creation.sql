ba464faa3d37b9b1cb6bb06afce13eef94686be5
ricardoomrques <ricarmarques26@outlook.pt>
2022-05-09 15:28:54 +0100

    types created in sqldeveloper

--- proj2/creation.sql
+++ proj2/creation.sql
@@ -1,23 +1,38 @@
-create or replace type coutries_t as object {
-    country_id char(2), 
-    region_name varchar2(25)
-}
+drop type regions_t;
+drop type countries_t;
+drop type locations_t;
+drop type employees_t;
+drop type jobs_t;
+drop type job_history_t;
+drop type departments_t; 
 
-create or replace type regions_t as object {
+create or replace type regions_t;
+create or replace type countries_t;
+create or replace type locations_t;
+create or replace type employees_t;
+create or replace type jobs_t;
+create or replace type job_history_t;
+create or replace type departments_t;
+
+create or replace type regions_t as object (
     region_id number,
-    region_name: varchar2,
-    countries countries_t
-}
+    region_name varchar2(25)
+);
 
-create or replace type locations_t as object {
+create or replace type countries_t as object (
+    country_id char(2), 
+    region_name varchar2(25),
+    region REF regions_t
+);
+
+create or replace type locations_t as object (
     location_id number(4), 
     street_addreess varchar2(40),
     postal_code varchar2(12),
     city varchar2(30),
-    state_province(25),
-    countries countries_t
-}
-
+    state_province varchar2(25),
+    countries REF countries_t
+);
 
 create or replace type employees_t as object (
     employee_id NUMBER(6),
@@ -26,16 +41,30 @@ create or replace type employees_t as object (
     email VARCHAR2(25),
     phone_number VARCHAR2(20),
     hire_date DATE,
-    job_id VARCHAR2(10),
-    salary NUMBER(8)
+    salary NUMBER(8),
     comission_pct NUMBER(2),
-    manager_id NUMBER(6),
-    department_id NUMBER(4)
-)
+    department REF departments_t,
+    jobs REF jobs_t
+);
 
 create or replace type jobs_t as object (
     job_id VARCHAR2(10),
     job_title VARCHAR2(35),
     min_salary NUMBER(6),
-    max_salary NUMBER(6)
-)
+    max_salary NUMBER(6),
+    job_history REF job_history_t
+);
+
+create or replace type job_history_t as object (
+    star_date DATE,
+    end_date DATE,
+    department REF departments_t,
+    employee REF employees_t
+);
+
+create or replace type departments_t as object (
+    department_id NUMBER(4),
+    department_name VARCHAR2(30),
+    employee REF employees_t,
+    locations REF locations_t
+);
