ee0d09eca83c71119dd56d38efa65648aaeeb007
ricardoomrques26 <ricardoomrques26@outlook.com>
2022-06-08 21:32:42 +0100

    query b for mongodb done

--- proj3/mongodb/queries.sql
+++ proj3/mongodb/queries.sql
@@ -6,3 +6,8 @@ db.cultural_facilities.aggregate(
     {"$match":{"municipalities.facilities.roomtype":/touros/,
     "municipalities.facilities.activities":"teatro"}},
     {"$project":{"municipalities.facilities._id":1,"municipalities.facilities.name":1,"municipalities.facilities.activities":1,"_id":0}})
+
+--QUESTION B--
+
+db.cultural_facilities.aggregate({"$unwind": "$municipalities"},{"$unwind": "$municipalities.facilities"}, {"$match":{"municipalities.facilities.roomtype":/touros/}},
+{$group : {_id:"$municipalities.region", count:{$sum:1}}})
