c631b94b2807f7a6d93a550009bbd223dbaf4eda
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-06-09 13:19:04 +0100

    add: sql query c

--- proj3/sql/queries.sql
+++ proj3/sql/queries.sql
@@ -17,4 +17,26 @@ inner join regions r on r.cod = m.region
 where r.description like '%touros%'
 group by r.designation;
 
+-- QUESTION C -- 
 
+-- better approach 
+select  c.counter - count(distinct m.cod) as non_cinema
+from facilities f
+inner join uses u on u.id = f.id 
+inner join activities a on u.ref = a.ref 
+inner join municipalities m on f.municipality = m.cod
+cross join (select count(*) as counter from municipalities)  c
+where a.activity = 'cinema'
+group by counter;
+
+-- second approach 
+select count(*) - c.has_cinema as hasnt_cinema
+from municipalities
+cross join (
+select count(distinct m.cod) as has_cinema
+from facilities f
+inner join uses u on u.id = f.id 
+inner join activities a on u.ref = a.ref 
+inner join municipalities m on f.municipality = m.cod
+where a.activity = 'cinema') c
+group by c.has_cinema;
