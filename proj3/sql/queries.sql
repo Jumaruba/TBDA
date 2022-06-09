e1926fd2a6ee845af271baeab900e1fc98cb46c2
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-06-09 12:37:40 +0100

    add: sql query b

--- proj3/sql/queries.sql
+++ proj3/sql/queries.sql
@@ -8,3 +8,13 @@ inner join activities a on a.ref = u.ref
 where r.description like '%touros%' and a.activity='teatro'; 
 
 
+-- QUESTION B -- 
+select r.designation, count(f.name)
+from facilities f 
+inner join roomtypes r on r.roomtype = f.roomtype 
+inner join municipalities m on m.cod = f.municipality 
+inner join regions r on r.cod = m.region 
+where r.description like '%touros%'
+group by r.designation;
+
+
